dd5ff61c302d7bdf50b832839f1aecb253e1e261
Elad Karako <eladkarako@users.noreply.github.com>
2017-02-26 20:12:34 +0200

    cleanup, updating dom-element through a timer..

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 /* Main 
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░*/
 
-(function(window){ "use strict";
-}(
-  self
-));
-
-/* ------------------------------------------------------------------------------------------ */
-
 (function(window, document, index, uploader, placer, worker){ "use strict";
   FileList.prototype.forEach = NodeList.prototype.forEach = Array.prototype.forEach;
 
-  function human_readable_bytes_size(bytes, digits, sap, is_comma_sap) {  "use strict";
-    digits       = "number"  === typeof digits        ? digits         :  2;
-    sap          = "string"  === typeof sap           ? sap            : "";
-    is_comma_sap = "boolean" === typeof is_comma_sap  ? is_comma_sap   : true;
+  function human_readable_bytes_size(bytes, digits, sap, is_comma_sap, is_smaller_factor) {  "use strict";
+    digits            = "number"  === typeof digits             ? digits             :  2;
+    sap               = "string"  === typeof sap                ? sap                : "";
+    is_comma_sap      = "boolean" === typeof is_comma_sap       ? is_comma_sap       : true;
+    is_smaller_factor = "boolean" === typeof is_smaller_factor  ? is_smaller_factor  : true;
 
     var  size = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
        , factor = Math.floor(  (String(bytes).length - 1) / 3  )
        ;
 
+    if(true === is_smaller_factor && factor > 0) factor-=1;  //more verbose to use a one mesurement less than maximum (1gb -> 1,024.00mb)
+
     bytes = bytes / Math.pow(1024, factor);  //calc
-    bytes = bytes - 1 ; //more verbose to use a one mesurement less than maximum (1gb -> 1,024.00mb)
     bytes = Math.floor(bytes * Math.pow(10, digits)) / Math.pow(10, digits);  //round digits
     
     if(true === is_comma_sap){
@@ -61,13 +56,16 @@
           textarea.value = "(..in progress..)";
         }else
         if("read_event_progress"  === message.data.message_reason){
-          textarea.value = "(..in progress..[" + ((message.data.loaded/message.data.total)*100) + "%]..)";
+            textarea.value = "(..in progress..[" + ((message.data.loaded/message.data.total)*100) + "%]..)";
         }else
         if("read_event_error"     === message.data.message_reason){
           textarea.value = "ERROR!";
         }else
         if("read_event_load"      === message.data.message_reason){
-          textarea.value = "data:" + message.data.type + ";base64," + message.data.result;
+          textarea.value = "(..wait...updating large textual-content..)";
+          setTimeout(function(){
+            textarea.value = "data:" + message.data.type + ";base64," + message.data.result;
+          },150);
         }
       };
       worker.postMessage({"message_reason":"read_file", "file":file});
