535e81af73457fd8cbf74851beee9d23123c7ad5
Elad Karako <eladkarako@users.noreply.github.com>
2017-02-26 19:37:08 +0200

    simplified with standard base64 conversion on main thread (may hang..) and built-in readAsBinaryString (which is not available in IE). legacy files moved to under '/res/' folder.

--- index.js
+++ index.js
@@ -1,28 +1,75 @@
 /* Main 
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░*/
 
-(function(window, document, uploader, placer, worker){ "use strict";
+(function(window){ "use strict";
+}(
+  self
+));
+
+/* ------------------------------------------------------------------------------------------ */
+
+(function(window, document, index, uploader, placer, worker){ "use strict";
   FileList.prototype.forEach = NodeList.prototype.forEach = Array.prototype.forEach;
 
-  uploader.onchange = function(){
-    
-    uploader.files.forEach(function(file){
-      worker = new Worker("worker.js");
+  function human_readable_bytes_size(bytes, digits, sap, is_comma_sap) {  "use strict";
+    digits       = "number"  === typeof digits        ? digits         :  2;
+    sap          = "string"  === typeof sap           ? sap            : "";
+    is_comma_sap = "boolean" === typeof is_comma_sap  ? is_comma_sap   : true;
 
+    var  size = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+       , factor = Math.floor(  (String(bytes).length - 1) / 3  )
+       ;
+
+    bytes = bytes / Math.pow(1024, factor);  //calc
+    bytes = bytes - 1 ; //more verbose to use a one mesurement less than maximum (1gb -> 1,024.00mb)
+    bytes = Math.floor(bytes * Math.pow(10, digits)) / Math.pow(10, digits);  //round digits
+    
+    if(true === is_comma_sap){
+      bytes = String(bytes).split(".");
+      bytes[0] = bytes[0].replace(/(\d)(?=(\d{3})+$)/g, "$1,");
+      bytes = bytes.join(".");
+    }
+
+    return String(bytes) + sap + size[factor];
+  }
+
+
+  index    = 0;
+
+  uploader.onchange = function(){
+
+    uploader.files.forEach(function(file){
+      var div, textarea;
+      index+=1;
+      div = document.createElement("div")
+      div.innerHTML = '<label for="#IDTEXT#">#INDEX#. #FNAME# [#FSIZE#]</label><textarea id="#IDTEXT#" readonly placeholder="#PLACEHOLDER#"></textarea>'
+                        .replace(/#IDTEXT#/g,       "text_" + index                      )
+                        .replace(/#INDEX#/g,        String(index)                        )
+                        .replace(/#FNAME#/g,        file.name                            )
+                        .replace(/#FSIZE#/g,        human_readable_bytes_size(file.size) )
+                        .replace(/#PLACEHOLDER#/g,  "data:" + ("" === file.type ? "application/octet-stream" : file.type) + ";base64," )
+                        ;
+      placer.appendChild(div);
+      textarea = div.querySelector("textarea");
+
+
+      worker = new Worker("worker.js");
       worker.onmessage = function(message){
         console.log(message);
-
-        if("read_event_loadstart" === message.data.message_reason){}
-        if("read_event_progress"  === message.data.message_reason){}
+        if("read_event_loadstart" === message.data.message_reason){
+          textarea.placeholder = "data:" + message.data.type + ";base64," //more accurate type.
+          textarea.value = "(..in progress..)";
+        }else
+        if("read_event_progress"  === message.data.message_reason){
+          textarea.value = "(..in progress..[" + ((message.data.loaded/message.data.total)*100) + "%]..)";
+        }else
+        if("read_event_error"     === message.data.message_reason){
+          textarea.value = "ERROR!";
+        }else
         if("read_event_load"      === message.data.message_reason){
-          placer.innerHTML += "<hr/><textarea readonly>" 
-                                      + "data:" + message.data.type + ";" 
-                                    //+ "charset:UTF-8" + ";"
-                                      + "base64," + message.data.result
-                                  + "</textarea>";
+          textarea.value = "data:" + message.data.type + ";base64," + message.data.result;
         }
       };
-
       worker.postMessage({"message_reason":"read_file", "file":file});
     });
   };
@@ -31,6 +78,7 @@
 }(
   self
  ,self.document
+ ,null
  ,self.document.querySelector('[id="uploader"]')
  ,self.document.querySelector("[placer]")
  ,null
--- worker.js
+++ worker.js
@@ -1,8 +1,6 @@
 /* Worker 
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░*/
 
-self.importScripts('_array_buffer_to_binary_string.js');
-self.importScripts('_binary_string_to_base64.js');
 self.importScripts('_file_extension_to_mime_type.js');
 
 function read_file(file){
@@ -10,12 +8,12 @@ function read_file(file){
       ,type          = get_type(file)
       ;
 
-  file_reader.onloadstart = function(ev){ self.postMessage({"message_reason":"read_event_loadstart",  "file":file,  "type": type,                                                                                                                          });  };
-  file_reader.onprogress  = function(ev){ self.postMessage({"message_reason":"read_event_progress",   "file":file,  "type": type,                                                                "loaded":ev.loaded,  "total":ev.total                     });  };
-  file_reader.onerror     = function(ev){ self.postMessage({"message_reason":"read_event_error",      "file":file,  "type": type,                                                                "loaded":ev.loaded,  "total":ev.total,  "error":ev.error  });  };
-  file_reader.onload      = function(ev){ self.postMessage({"message_reason":"read_event_load",       "file":file,  "type": type,  "result": file_reader.result.to_binary_string().to_base64(),  "loaded":ev.loaded,  "total":ev.total                     });  };
+  file_reader.onloadstart = function(ev){ self.postMessage({"message_reason":"read_event_loadstart",  "file":file,  "type": type,                                                                                                 });  };
+  file_reader.onprogress  = function(ev){ self.postMessage({"message_reason":"read_event_progress",   "file":file,  "type": type,                                       "loaded":ev.loaded,  "total":ev.total                     });  };
+  file_reader.onerror     = function(ev){ self.postMessage({"message_reason":"read_event_error",      "file":file,  "type": type,                                       "loaded":ev.loaded,  "total":ev.total,  "error":ev.error  });  };
+  file_reader.onload      = function(ev){ self.postMessage({"message_reason":"read_event_load",       "file":file,  "type": type,  "result": btoa(file_reader.result),  "loaded":ev.loaded,  "total":ev.total                     });  };
 
-  file_reader.readAsArrayBuffer(file);
+  file_reader.readAsBinaryString(file);
 }
 
 self.onmessage = function(message){
