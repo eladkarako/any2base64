8a304c60c66c594c5c453cb31f73ff287c4b8a2e
Elad Karako <eladkarako@users.noreply.github.com>
2017-03-08 14:41:46 +0200

    clean/move code around from main-code-block.

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 /* Main 
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░*/
 
-(function(window, document, uploader, placer, index, worker){ "use strict";
-  FileList.prototype.forEach = NodeList.prototype.forEach = Array.prototype.forEach;
+(function(window){                                                                       "use strict";
+  window.FileList.prototype.forEach = window.NodeList.prototype.forEach = window.Array.prototype.forEach;
+}(self));
 
-  function human_readable_bytes_size(bytes, digits, sap, is_comma_sap, is_smaller_factor) {  "use strict";
-    digits            = "number"  === typeof digits             ? digits             :  2;
-    sap               = "string"  === typeof sap                ? sap                : "";
-    is_comma_sap      = "boolean" === typeof is_comma_sap       ? is_comma_sap       : true;
-    is_smaller_factor = "boolean" === typeof is_smaller_factor  ? is_smaller_factor  : true;
-
-    var  size = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
-       , factor = Math.floor(  (String(bytes).length - 1) / 3  )
-       ;
-
-    if(true === is_smaller_factor && factor > 0) factor-=1;  //more verbose to use a one mesurement less than maximum (1gb -> 1,024.00mb)
-
-    bytes = bytes / Math.pow(1024, factor);  //calc
-    bytes = Math.floor(bytes * Math.pow(10, digits)) / Math.pow(10, digits);  //round digits
-    
-    if(true === is_comma_sap){
-      bytes = String(bytes).split(".");
-      bytes[0] = bytes[0].replace(/(\d)(?=(\d{3})+$)/g, "$1,");
-      bytes = bytes.join(".");
-    }
-
-    return String(bytes) + sap + size[factor];
-  }
+(function(window, document, human_readable_bytes_size, uploader, placer, index, worker){ "use strict";
 
   uploader.onchange = function(){
 
@@ -78,10 +57,33 @@
 
   
 }(
-  /* window     */   self
- ,/* document   */   self.document
- ,/* uploader   */   self.document.querySelector('[id="uploader"]')
- ,/* placer     */   self.document.querySelector("[placer]")
- ,/* index      */   0
- ,/* worker     */   null
+  /* window                     */   self
+ ,/* document                   */   self.document
+ ,/* human_readable_bytes_size  */   function(bytes, digits, sap, is_comma_sap, is_smaller_factor){  "use strict";
+                                       digits            = "number"  === typeof digits             ? digits             :  2;
+                                       sap               = "string"  === typeof sap                ? sap                : "";
+                                       is_comma_sap      = "boolean" === typeof is_comma_sap       ? is_comma_sap       : true;
+                                       is_smaller_factor = "boolean" === typeof is_smaller_factor  ? is_smaller_factor  : true;
+
+                                       var  size = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+                                          , factor = Math.floor(  (String(bytes).length - 1) / 3  )
+                                          ;
+
+                                       if(true === is_smaller_factor && factor > 0) factor-=1;  //more verbose to use a one mesurement less than maximum (1gb -> 1,024.00mb)
+
+                                       bytes = bytes / Math.pow(1024, factor);  //calc
+                                       bytes = Math.floor(bytes * Math.pow(10, digits)) / Math.pow(10, digits);  //round digits
+                                      
+                                       if(true === is_comma_sap){
+                                         bytes = String(bytes).split(".");
+                                         bytes[0] = bytes[0].replace(/(\d)(?=(\d{3})+$)/g, "$1,");
+                                         bytes = bytes.join(".");
+                                       }
+
+                                       return String(bytes) + sap + size[factor];
+                                     }
+ ,/* uploader                   */   self.document.querySelector('[id="uploader"]')
+ ,/* placer                     */   self.document.querySelector("[placer]")
+ ,/* index                      */   0
+ ,/* worker                     */   null
 ));
